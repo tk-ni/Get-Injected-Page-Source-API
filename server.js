d52c25ae82666ef8bdf8f8ce228114a7eb18ee72
tk-ni <talkalepanagos@gmail.com>
2021-01-11 20:43:37 +0200

    added nodemon

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ const mainRouter = require('./routes/main.route');
 app.use('/', mainRouter);
 
 app.listen(process.env.PORT || 3201, ()=>{
-    console.log('Server listening to ', process.env.PORT || 3201);
+    console.log('Server listening to', process.env.PORT || 3201);
 })
